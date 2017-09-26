31a1332f40d76f3e09cf7a742d41ca0d54371aaf
yann marquet <ymarquet@gmail.com>
2017-09-26 20:09:34 +0100

    env var for elasticsearch

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,3 @@
 require 'elasticsearch/rails/instrumentation/log_subscriber'
 
-Elasticsearch::Model.client = Elasticsearch::Client.new host: 'elasticsearch:9200', log: true
+Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV.fetch('ELASTIC_SEARCH_URL', 'elasticsearch:9200'), log: true
