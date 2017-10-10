213fc1522b9b246e88604c7cc99684b3234c5e5f
yann marquet <ymarquet@gmail.com>
2017-10-10 20:14:12 +0100

    yeah case sensitovity fuckup

---
+++ app/javascript/components/Articles/ListItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom';
+
+export default class ListItem extends React.Component {
+
+  render() {
+    return(
+      <tr>
+        <td>{this.props.article.created_at}</td>
+        <td>
+          <Link className="small" to={`/admin/articles/${this.props.article.id}`} >
+            {this.props.article.title}
+          </Link>
+        </td>
+        <td>
+          <button className="button small">
+            <i className="fa fa-check"/>
+            Unpublish
+          </button>
+
+        </td>
+      </tr>
+    )
+  }
+}
